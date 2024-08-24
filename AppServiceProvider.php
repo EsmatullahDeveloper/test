e1699ea01a42b383c4b8cd5e148f257cfe92c8be
EsmatullahDeveloper <esmatullaherfani313@gmail.com>
2024-08-24 12:14:44 -0700

    jjal
    I am here

---
+++ AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+// use Barryvdh\DomPDF\ServiceProvider as DomPDFServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // $this->app->register(DomPDFServiceProvider::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Paginator::useBootstrap();
+    }
+}
